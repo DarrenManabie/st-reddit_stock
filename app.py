80b0e949f05e80008184a8e611aa46da1e0ffd5c
DarrenManabie <118432717+DarrenManabie@users.noreply.github.com>
2024-08-04 22:06:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -118,8 +118,9 @@ def analyze_posts(subPopular, subToday):
     return results
 
 def main():
-    st.title(f"Reddit r/stocks summarizer")
-
+    today = datetime.today().strftime('%-d %B %Y')
+    st.title("Reddit r/stocks summarizer")
+    st.subheader(f"Date: {today}")
     
     if st.button("Fetch and Analyze Posts"):
         with st.spinner("Fetching posts from Reddit..."):
